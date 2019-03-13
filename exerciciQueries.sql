7cd4718f5b28c9d4b2dffdbeeef2b3b5735cfe49
etdisseny <47816444+etdisseny@users.noreply.github.com>
2019-03-13 12:13:30 +0100

    Add files via upload

--- exerciciQueries.sql
+++ exerciciQueries.sql
@@ -6,17 +6,19 @@ SELECT Origin, AVG(ArrDelay), AVG(DepDelay) FROM flights
 GROUP BY Origin;
 
 /*3*/
-SELECT Origin, AVG(ArrDelay) as retard, colMonth FROM flights
-GROUP BY Origin, colMonth
-ORDER BY Origin, colMonth;
+SELECT Origin, colYear, colMonth, AVG(ArrDelay) as retard FROM flights
+GROUP BY Origin, colYear, colMonth
+ORDER BY Origin, colYear, colMonth;
 
 /*4*/
-SELECT  a.City, AVG(f.ArrDelay) as retard, f.colMonth
+SELECT  a.City, f.colYear, f.colMonth, AVG(f.ArrDelay) as retard
 FROM flights f
-INNER JOIN usairports a
+LEFT JOIN usairports a
 ON f.Origin = a.IATA
-GROUP BY a.City, f.colMonth
-ORDER BY a.City, retard, f.colMonth;
+GROUP BY a.City, f.colYear, f.colMonth
+ORDER BY a.City, f.colYear, f.colMonth;
+
+
 
 /*5*/
 SELECT c.Description, f.cancelled 
@@ -24,12 +26,43 @@ FROM flights f
 INNER JOIN carriers c
 ON f.UniqueCarrier = c.CarrierCode
 WHERE f.cancelled >0
+GROUP BY c.Description
 ORDER BY f.cancelled desc;
 
+/*5 new */
+SELECT UniqueCarrier, sum(cancelled) as cancelados
+FROM flights f
+GROUP BY UniqueCarrier;
+
+
+SELECT c.Description, sum(f.cancelled) as cancelados
+FROM flights f
+INNER JOIN carriers c
+ON f.UniqueCarrier = c.CarrierCode
+WHERE cancelados >0
+GROUP BY c.Description
+ORDER BY cancelados;
+
+/*5 ok*/
+
+SELECT c.Description, sum(f.cancelled) as cancelados
+FROM flights f
+INNER JOIN carriers c
+ON f.UniqueCarrier = c.CarrierCode
+GROUP BY c.Description
+HAVING cancelados >0
+ORDER BY cancelados desc;
+
+
+
+
 /*6*/
-SELECT flightiD, Distance FROM flights
+SELECT flightNum, Distance FROM flights
+GROUP BY flightNum
 ORDER BY Distance desc
 LIMIT 10;
+
+
 
 /*7*/
 SELECT UniqueCarrier, avg(ArrDelay) as retras from flights
